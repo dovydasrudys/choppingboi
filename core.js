783fc2af536016e80014969760b07edc3d5185c5
Dovydas <dovydas.rudys@indeform.com>
2021-06-22 15:40:01 +0300

    Remove outdated code

--- core.js
+++ core.js
@@ -1,7 +1,5 @@
 import 'dotenv/config.js';
 
-import fs from 'fs';
-
 import Discord from 'discord.js';
 import axios from 'axios';
 import pkg from 'pg';
@@ -20,10 +18,24 @@ const postgres = new Client({
 
 postgres.connect();
 
+//Create tables
 postgres.query('CREATE TABLE IF NOT EXISTS servers (id VARCHAR ( 255 ) PRIMARY KEY, created_on TIMESTAMP NOT NULL);', (err, res) => {
     if (err) throw err;
 });
 
+postgres.query('CREATE TABLE IF NOT EXISTS posts (id VARCHAR ( 255 ) PRIMARY KEY, created_on TIMESTAMP NOT NULL);', (err, res) => {
+    if (err) throw err;
+});
+
+postgres.query('CREATE TABLE IF NOT EXISTS logs (message VARCHAR ( 255 ), created_on TIMESTAMP NOT NULL);', (err, res) => {
+    if (err) throw err;
+});
+
+postgres.query('CREATE TABLE IF NOT EXISTS filters (id SERIAL, filter VARCHAR ( 255 ) UNIQUE, created_on TIMESTAMP NOT NULL);', (err, res) => {
+    if (err) throw err;
+});
+
+//Server functions
 const insertServer = async (serverId) => {
     postgres.query(`INSERT INTO servers (id, created_on) VALUES ('${serverId}', to_timestamp(${Date.now() / 1000}));`, (err, res) => {
         if (err) throw err;
@@ -36,10 +48,7 @@ const getServers = async () => {
     return res.rows;
 }
 
-postgres.query('CREATE TABLE IF NOT EXISTS posts (id VARCHAR ( 255 ) PRIMARY KEY, created_on TIMESTAMP NOT NULL);', (err, res) => {
-    if (err) throw err;
-});
-
+//Post functions
 const insertPost = async (postId) => {
     postgres.query(`INSERT INTO posts (id, created_on) VALUES ('${postId}', to_timestamp(${Date.now() / 1000}));`, (err, res) => {
         if (err) throw err;
@@ -51,20 +60,14 @@ const getLastPostFromDB = async () => {
     return res.rows[0].id;
 };
 
-postgres.query('CREATE TABLE IF NOT EXISTS logs (message VARCHAR ( 255 ), created_on TIMESTAMP NOT NULL);', (err, res) => {
-    if (err) throw err;
-});
-
+//Log functions
 const insertLog = async (message) => {
     postgres.query(`INSERT INTO logs (message, created_on) VALUES ('${message}', to_timestamp(${Date.now() / 1000}));`, (err, res) => {
         if (err) throw err;
     });
 }
 
-postgres.query('CREATE TABLE IF NOT EXISTS filters (id SERIAL, filter VARCHAR ( 255 ) UNIQUE, created_on TIMESTAMP NOT NULL);', (err, res) => {
-    if (err) throw err;
-});
-
+//Filter functions
 const insertFilter = async (filter) => {
     postgres.query(`INSERT INTO filters (filter, created_on) VALUES ('${filter}', to_timestamp(${Date.now() / 1000}));`, (err, res) => {
         if (err) throw err;
@@ -97,7 +100,6 @@ let lastPost = {
     id: null
 };
 
-//lastPost.id = fs.readFileSync('last_post.txt', { encoding: 'utf-8', flag: 'r' });
 lastPost.id = await getLastPostFromDB();
 
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
@@ -132,17 +134,11 @@ client.on('message', (message) => {
 async function logLine(line) {
     let datetime = new Date();
     datetime.setHours(datetime.getHours() + 3);
-    // fs.appendFileSync('log.txt', `${datetime.toLocaleString()}: ${line}\n`);
     await insertLog(`${line}`);
 }
 
 async function showFilters(message) {
     const filters = await getFilters();
-    // const filtersContent = fs.readFileSync('filters.txt', { encoding: 'utf8', flag: 'r' }).trim();
-    // if (filtersContent === '') {
-    //     return;
-    // }
-    // const filters = filtersContent.split('\n');
     let filtersMessage = '';
 
     for (let index = 0; index < filters.length; index++) {
@@ -155,25 +151,12 @@ async function addFilter(message) {
     const newFilter = message.content.toLowerCase().replace('!chop filters add ', '');
     await insertFilter(newFilter);
     message.channel.send(`"${newFilter}" added to filters list.`);
-    // fs.appendFile('filters.txt', `${newFilter}\n`, () => {
-    //     message.channel.send(`"${newFilter}" added to filters list.`);
-    // });
 }
 
 async function removeFilter(message) {
     const filterToDeleteId = parseInt(message.content.toLowerCase().replace('!chop filters remove ', ''));
     await deleteFilter(filterToDeleteId);
     message.channel.send(`Filter No.${filterToDeleteId} deleted from filters list.`);
-    // const filtersContent = fs.readFileSync('filters.txt', { encoding: 'utf8', flag: 'r' }).trim();
-    // const updatedFilters = filtersContent.split('\n').filter((value, index) => index !== filterToDeleteNr - 1);
-    // await fs.writeFile('filters.txt', '', () => {
-    //     console.log('filters file cleaned');
-    // });
-    // for (let index = 0; index < updatedFilters.length; index++) {
-    //     fs.appendFile('filters.txt', `${updatedFilters[index]}\n`, () => {
-    //         message.channel.send(`Filter No.${filterToDeleteNr} deleted from filters list.`);
-    //     });
-    // }
 }
 
 async function registerChannel(message) {
@@ -184,19 +167,6 @@ async function registerChannel(message) {
     catch {
         message.channel.send('This channel was already in my list 😊');
     }
-
-    // message.channel.send('I added this channel to my list 😊');
-    // const content = fs.readFileSync('channels.txt', { encoding: 'utf8', flag: 'r' });
-    // const ids = content.split('\n');
-    // if (!ids.includes(message.channel.id)) {
-    //     fs.appendFile('channels.txt', `${message.channel.id}\n`, function (err) {
-    //         if (err) console.log(err);
-    //         console.log(`${message.channel.id} added to channels list`);
-    //         message.channel.send('I added this channel to my list 😊');
-    //     });
-    // } else {
-    //     message.channel.send('This channel was already in my list 😊');
-    // }
 }
 
 function addChopRequest(message) {
@@ -314,10 +284,7 @@ async function checkSubreddit() {
         logLine(`New post was found (${post.id}). Last post was (${lastPost.id}).`);
         lastPost = post;
         insertPost(post.id);
-        // fs.writeFileSync('last_post.txt', post.id);
         const possibleReactions = await getPossibleCutReactions(post.url);
-        // const content = fs.readFileSync('channels.txt', { encoding: 'utf8', flag: 'r' });
-        // const ids = content.split('\n');
         const servers = await getServers();
         const ids = servers.map(server => server.id);
         // ids.pop();
@@ -328,10 +295,6 @@ async function checkSubreddit() {
                 .addField('Image title', `${post.title.replace(/[^a-zA-Z0-9]/g, '_')}`)
                 .addField('Original image url', post.url)
                 .addField('Reddit post', `${SUBREDDIT}/comments/${post.id}`);
-
-            // let datetime = new Date();
-            // datetime.setHours(datetime.getHours() + 3);
-            // fs.appendFileSync('posts.txt', `${datetime.toLocaleString()}: ${post.id}\n`);
 
             ids.forEach((id) => {
                 const channel = client.channels.cache.get(id);
@@ -357,8 +320,6 @@ async function getLastPost() {
         if (response.data.data.children.length > 0) {
             const post = response.data.data.children[0].data;
             if (post && post.id !== lastPost.id) {
-                // const filtersContent = fs.readFileSync('filters.txt', { encoding: 'utf8', flag: 'r' }).trim();
-                // const filters = filtersContent.split('\n');
                 const filtersRes = await getFilters();
                 const filters = filtersRes.map(filter => filter.filter);
                 filters.forEach((filter) => {
