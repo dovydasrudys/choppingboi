a09eb3f50ef0128b75cc685c601f08b1df215928
Dovydas <dovydas.rudys@indeform.com>
2021-06-22 13:25:27 +0300

    Use postgres

--- core.js
+++ core.js
@@ -4,9 +4,85 @@ import fs from 'fs';
 
 import Discord from 'discord.js';
 import axios from 'axios';
+import pkg from 'pg';
+const { Client } = pkg;
 
 import { chop, getPossibleCutReactions } from './chopper.js';
 import { reactionOptions } from './reactionOptions.js';
+
+//Connect to postgresql
+const postgres = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+postgres.connect();
+
+postgres.query('CREATE TABLE IF NOT EXISTS servers (id VARCHAR ( 255 ) PRIMARY KEY, created_on TIMESTAMP NOT NULL);', (err, res) => {
+    if (err) throw err;
+});
+
+const insertServer = async (serverId) => {
+    postgres.query(`INSERT INTO servers (id, created_on) VALUES ('${serverId}', to_timestamp(${Date.now() / 1000}));`, (err, res) => {
+        if (err) throw err;
+    });
+}
+
+const getServers = async () => {
+    const res = await postgres.query(`SELECT * FROM servers ORDER BY created_on ASC;`);
+
+    return res.rows;
+}
+
+postgres.query('CREATE TABLE IF NOT EXISTS posts (id VARCHAR ( 255 ) PRIMARY KEY, created_on TIMESTAMP NOT NULL);', (err, res) => {
+    if (err) throw err;
+});
+
+const insertPost = async (postId) => {
+    postgres.query(`INSERT INTO posts (id, created_on) VALUES ('${postId}', to_timestamp(${Date.now() / 1000}));`, (err, res) => {
+        if (err) throw err;
+    });
+}
+
+const getLastPostFromDB = async () => {
+    const res = await postgres.query('SELECT max(id) FROM posts;');
+    return res.rows[0].max;
+};
+
+postgres.query('CREATE TABLE IF NOT EXISTS logs (message VARCHAR ( 255 ), created_on TIMESTAMP NOT NULL);', (err, res) => {
+    if (err) throw err;
+});
+
+const insertLog = async (message) => {
+    postgres.query(`INSERT INTO logs (message, created_on) VALUES ('${message}', to_timestamp(${Date.now() / 1000}));`, (err, res) => {
+        if (err) throw err;
+    });
+}
+
+postgres.query('CREATE TABLE IF NOT EXISTS filters (id SERIAL, filter VARCHAR ( 255 ) UNIQUE, created_on TIMESTAMP NOT NULL);', (err, res) => {
+    if (err) throw err;
+});
+
+const insertFilter = async (filter) => {
+    postgres.query(`INSERT INTO filters (filter, created_on) VALUES ('${filter}', to_timestamp(${Date.now() / 1000}));`, (err, res) => {
+        if (err) throw err;
+    });
+}
+
+const getFilters = async () => {
+    const res = await postgres.query(`SELECT * FROM filters ORDER BY id ASC;`)
+
+    return res.rows;
+}
+
+const deleteFilter = async (id) => {
+    postgres.query(`DELETE FROM filters WHERE id = ${id};`, (err, res) => {
+        if (err) throw err;
+    });
+}
+
 
 //Set up express
 import express from 'express';
@@ -21,7 +97,8 @@ let lastPost = {
     id: null
 };
 
-lastPost.id = fs.readFileSync('last_post.txt', { encoding: 'utf-8', flag: 'r' });
+//lastPost.id = fs.readFileSync('last_post.txt', { encoding: 'utf-8', flag: 'r' });
+lastPost.id = await getLastPostFromDB();
 
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
@@ -46,64 +123,80 @@ client.on('message', (message) => {
     } else if (message.content.toLowerCase().startsWith('!chop filters add')) {
         addFilter(message);
     } else if (message.content.toLowerCase().startsWith('!chop filters remove')) {
-        removeFilter();
+        removeFilter(message);
     } else if (message.content.toLowerCase().startsWith('!chop')) {
         addChopRequest(message);
     }
 });
 
-function logLine(line) {
+async function logLine(line) {
     let datetime = new Date();
     datetime.setHours(datetime.getHours() + 3);
-    fs.appendFileSync('log.txt', `${datetime.toLocaleString()}: ${line}\n`);
+    // fs.appendFileSync('log.txt', `${datetime.toLocaleString()}: ${line}\n`);
+    await insertLog(`${line}`);
 }
 
-function showFilters(message) {
-    const filtersContent = fs.readFileSync('filters.txt', { encoding: 'utf8', flag: 'r' }).trim();
-    if (filtersContent === '') {
-        return;
-    }
-    const filters = filtersContent.split('\n');
+async function showFilters(message) {
+    const filters = await getFilters();
+    // const filtersContent = fs.readFileSync('filters.txt', { encoding: 'utf8', flag: 'r' }).trim();
+    // if (filtersContent === '') {
+    //     return;
+    // }
+    // const filters = filtersContent.split('\n');
     let filtersMessage = '';
+
     for (let index = 0; index < filters.length; index++) {
-        filtersMessage += `[${index + 1}] ${filters[index]}\n`;
+        filtersMessage += `[${filters[index].id}] ${filters[index].filter}\n`;
     }
     message.channel.send(filtersMessage);
 }
 
-function addFilter(message) {
+async function addFilter(message) {
     const newFilter = message.content.toLowerCase().replace('!chop filters add ', '');
-    fs.appendFile('filters.txt', `${newFilter}\n`, () => {
-        message.channel.send(`"${newFilter}" added to filters list.`);
-    });
+    await insertFilter(newFilter);
+    message.channel.send(`"${newFilter}" added to filters list.`);
+    // fs.appendFile('filters.txt', `${newFilter}\n`, () => {
+    //     message.channel.send(`"${newFilter}" added to filters list.`);
+    // });
 }
 
 async function removeFilter(message) {
-    const filterToDeleteNr = parseInt(message.content.toLowerCase().replace('!chop filters remove ', ''));
-    const filtersContent = fs.readFileSync('filters.txt', { encoding: 'utf8', flag: 'r' }).trim();
-    const updatedFilters = filtersContent.split('\n').filter((value, index) => index !== filterToDeleteNr - 1);
-    await fs.writeFile('filters.txt', '', () => {
-        console.log('filters file cleaned');
-    });
-    for (let index = 0; index < updatedFilters.length; index++) {
-        fs.appendFile('filters.txt', `${updatedFilters[index]}\n`, () => {
-            message.channel.send(`Filter No.${filterToDeleteNr} deleted from filters list.`);
-        });
-    }
+    const filterToDeleteId = parseInt(message.content.toLowerCase().replace('!chop filters remove ', ''));
+    await deleteFilter(filterToDeleteId);
+    message.channel.send(`Filter No.${filterToDeleteId} deleted from filters list.`);
+    // const filtersContent = fs.readFileSync('filters.txt', { encoding: 'utf8', flag: 'r' }).trim();
+    // const updatedFilters = filtersContent.split('\n').filter((value, index) => index !== filterToDeleteNr - 1);
+    // await fs.writeFile('filters.txt', '', () => {
+    //     console.log('filters file cleaned');
+    // });
+    // for (let index = 0; index < updatedFilters.length; index++) {
+    //     fs.appendFile('filters.txt', `${updatedFilters[index]}\n`, () => {
+    //         message.channel.send(`Filter No.${filterToDeleteNr} deleted from filters list.`);
+    //     });
+    // }
 }
 
-function registerChannel(message) {
-    const content = fs.readFileSync('channels.txt', { encoding: 'utf8', flag: 'r' });
-    const ids = content.split('\n');
-    if (!ids.includes(message.channel.id)) {
-        fs.appendFile('channels.txt', `${message.channel.id}\n`, function (err) {
-            if (err) console.log(err);
-            console.log(`${message.channel.id} added to channels list`);
-            message.channel.send('I added this channel to my list 😊');
-        });
-    } else {
+async function registerChannel(message) {
+    try {
+        await insertServer(message.channel.id);
+        message.channel.send('I added this channel to my list 😊');
+    }
+    catch {
         message.channel.send('This channel was already in my list 😊');
     }
+
+    // message.channel.send('I added this channel to my list 😊');
+    // const content = fs.readFileSync('channels.txt', { encoding: 'utf8', flag: 'r' });
+    // const ids = content.split('\n');
+    // if (!ids.includes(message.channel.id)) {
+    //     fs.appendFile('channels.txt', `${message.channel.id}\n`, function (err) {
+    //         if (err) console.log(err);
+    //         console.log(`${message.channel.id} added to channels list`);
+    //         message.channel.send('I added this channel to my list 😊');
+    //     });
+    // } else {
+    //     message.channel.send('This channel was already in my list 😊');
+    // }
 }
 
 function addChopRequest(message) {
@@ -152,12 +245,10 @@ async function handleMessageReaction(reaction, user) {
     }
 
     if (user.id === client.user.id) {
-        console.log(user.id, client.user.id);
         return;
     }
 
     if (reaction.message.author.id === client.user.id && reaction.message.embeds.length > 0) {
-        console.log(reaction.message.author.id, client.user.id);
         //If not one of the possible reaction options then do nothing
         if (!Object.values(reactionOptions).includes(reaction.emoji.name)) {
             return;
@@ -215,81 +306,77 @@ async function handleMessageReaction(reaction, user) {
     logLine('Message reaction handled successfully');
 }
 
-function checkSubreddit() {
+async function checkSubreddit() {
     logLine('Checking SUBREDDIT');
-    getLastPost().then(post => {
-        if (post) {
-            logLine(`New post was found (${post.id}). Last post was (${lastPost.id}).`);
-            lastPost = post;
-            fs.writeFileSync('last_post.txt', post.id);
-            getPossibleCutReactions(post.url).then(possibleReactions => {
-                const content = fs.readFileSync('channels.txt', { encoding: 'utf8', flag: 'r' });
-                const ids = content.split('\n');
-                ids.pop();
-                if (ids.length > 0) {
-                    const embed = new Discord.MessageEmbed()
-                        .setTitle(`${post.title}`)
-                        .setImage(`${post.url}`)
-                        .addField('Image title', `${post.title.replace(/[^a-zA-Z0-9]/g, '_')}`)
-                        .addField('Original image url', post.url)
-                        .addField('Reddit post', `${SUBREDDIT}/comments/${post.id}`);
+    const post = await getLastPost();
 
-                    let datetime = new Date();
-                    datetime.setHours(datetime.getHours() + 3);
-                    fs.appendFileSync('posts.txt', `${datetime.toLocaleString()}: ${post.id}\n`);
+    if (post) {
+        logLine(`New post was found (${post.id}). Last post was (${lastPost.id}).`);
+        lastPost = post;
+        insertPost(post.id);
+        // fs.writeFileSync('last_post.txt', post.id);
+        const possibleReactions = await getPossibleCutReactions(post.url);
+        // const content = fs.readFileSync('channels.txt', { encoding: 'utf8', flag: 'r' });
+        // const ids = content.split('\n');
+        const servers = await getServers();
+        const ids = servers.map(server => server.id);
+        // ids.pop();
+        if (ids.length > 0) {
+            const embed = new Discord.MessageEmbed()
+                .setTitle(`${post.title}`)
+                .setImage(`${post.url}`)
+                .addField('Image title', `${post.title.replace(/[^a-zA-Z0-9]/g, '_')}`)
+                .addField('Original image url', post.url)
+                .addField('Reddit post', `${SUBREDDIT}/comments/${post.id}`);
 
-                    ids.forEach((id) => {
-                        const channel = client.channels.cache.get(id);
-                        if (channel) {
-                            channel.send(embed)
-                                .then(message => {
-                                    possibleReactions.forEach((option) => {
-                                        message.react(option);
-                                    });
-                                });
-                        }
-                    });
+            // let datetime = new Date();
+            // datetime.setHours(datetime.getHours() + 3);
+            // fs.appendFileSync('posts.txt', `${datetime.toLocaleString()}: ${post.id}\n`);
+
+            ids.forEach((id) => {
+                const channel = client.channels.cache.get(id);
+                if (channel) {
+                    channel.send(embed)
+                        .then(message => {
+                            possibleReactions.forEach((option) => {
+                                message.react(option);
+                            });
+                        });
                 }
-            })
-
-        } else {
-            logLine('There are no new posts.');
+            });
         }
-    }).catch(reason => {
-        logLine(`Catch block: ${reason}`);
-    });
+
+    } else {
+        logLine('There are no new posts.');
+    }
 }
 
-function getLastPost() {
-    return axios.get(`${SUBREDDIT}/new.json?limit=1`)
-        .then(response => {
-            if (response.status === 200) {
-                if (response.data.data.children.length > 0) {
-                    const post = response.data.data.children[0].data;
-                    if (post && post.id !== lastPost.id) {
-                        const filtersContent = fs.readFileSync('filters.txt', { encoding: 'utf8', flag: 'r' }).trim();
-                        const filters = filtersContent.split('\n');
-                        filters.forEach((filter) => {
-                            if (post.title.toLowerCase().includes(filter)) {
-                                return null;
-                            }
-                        });
-
-                        return post;
-                    } else {
+async function getLastPost() {
+    const response = await axios.get(`${SUBREDDIT}/new.json?limit=1`);
+    if (response.status === 200) {
+        if (response.data.data.children.length > 0) {
+            const post = response.data.data.children[0].data;
+            if (post && post.id !== lastPost.id) {
+                // const filtersContent = fs.readFileSync('filters.txt', { encoding: 'utf8', flag: 'r' }).trim();
+                // const filters = filtersContent.split('\n');
+                const filtersRes = await getFilters();
+                const filters = filtersRes.map(filter => filter.filter);
+                filters.forEach((filter) => {
+                    if (post.title.toLowerCase().includes(filter)) {
                         return null;
                     }
-                } else {
-                    return null;
-                }
+                });
+
+                return post;
             } else {
                 return null;
             }
-        })
-        .catch(reason => {
-            console.log(reason);
+        } else {
             return null;
-        });
+        }
+    } else {
+        return null;
+    }
 }
 
 client.login(process.env.DISCORD_TOKEN);
